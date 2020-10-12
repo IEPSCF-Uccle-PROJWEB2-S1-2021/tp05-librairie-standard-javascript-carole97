ca232adc3fd5682f21cc94242d85929c4eccd088
52060 <52060@etu.he2b.be git config --globla user.name carole>
2020-10-12 20:41:43 +0200

    tp05

--- exercice2.js
+++ exercice2.js
@@ -1,3 +1,25 @@
 function analyzeTemperature(quantity, data) {
-  return 0;
+  const tabData = data.split(' '); let verif = false;
+  if (quantity <= 0) {
+    return 0;
+  } else {
+    if (quantity <= 10000 && tabData.length === quantity) {
+      let i = 0;
+      while (i < tabData.length && verif === false) {
+        let element = parseInt(tabData[i]);
+        if (-273 > element || element > 5526)
+          verif = true;
+        if (Math.abs(element) <= Temperature) {
+          Temperature = element;
+        }
+        ++i;
+      } if (verif === true) {
+        Temperature =    " les temperatures doivent être comprisent entre -273 à 5526. ";
+      }
+    } else {
+      Temperature = "la quantité ne correspond pas ";
+    }
+  }
+  return Temperature;
+}
 }
